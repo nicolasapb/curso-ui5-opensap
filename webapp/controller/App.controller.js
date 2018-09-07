591b7ff904dfe7d2cbe7160724b172ba0940ecb6
Nicolas <nicolas.a.p.barbosa@gmail.com>
2018-09-07 10:31:00 -0300

    ui5 2.4

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,15 +1,17 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
+    'sap/m/GroupHeaderListItem',
     "sap/m/MessageToast",
+    'sap/ui/model/Filter',
     "./Formatter"
-], function(Controller, MessageToast, Formatter) {
+], function(Controller, GroupHeaderListItem, MessageToast, Filter, Formatter) {
     'use strict';
 
     Controller.extend("opensap.myapp.controller.App", {
 
         formatter: Formatter,
 
-        onShowHello: function() { 
+        onShowHello: function() {
 
             var oView = this.getView();
             var oBundle = oView.getModel('i18n').getResourceBundle();
@@ -17,6 +19,27 @@ sap.ui.define([
             var sMsg = oBundle.getText("helloMsg", [sRecipient]);
 
             MessageToast.show(sMsg);
+        },
+
+        getGroupHeader: function (oGroup){
+			return new GroupHeaderListItem( {
+				title: oGroup.key,
+				upperCase: false
+			} );
+		},
+
+        onFilterProducts: function(oEvent) {
+
+            var aFilters = [];
+            var sQuery = oEvent.getSource().getValue();
+            if (sQuery && sQuery.length > 0) {
+                var filter = new Filter("ProductID", sap.ui.model.FilterOperator.Contains, sQuery);
+                aFilters.push(filter);
+            }
+
+            var oList = this.byId("invoiceList");
+            var oItems = oList.getBinding("items");
+            oItems.filter(aFilters, "Application");
         }
     });
 });
--- webapp/serviceBinding.js
+++ webapp/serviceBinding.js
@@ -1,5 +1,5 @@
 function initModel() {
-	var sUrl = "/sapes5/sap/opu/odata/IWBEP/GWSAMPLE_BASIC/";
+	var sUrl = "/destinations/ES5/sap/opu/odata/IWBEP/GWSAMPLE_BASIC/";
 	var oModel = new sap.ui.model.odata.ODataModel(sUrl, true);
 	sap.ui.getCore().setModel(oModel);
 }
